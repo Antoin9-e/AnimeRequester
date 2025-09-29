21d5cd9163829b46d3391677abdd7666ec98fb18
Matter Antoine <antoine.matter@etu.unicaen.fr>
2025-09-29 21:30:37 +0200

    patch search function  and add style for the card

--- script.js
+++ script.js
@@ -73,8 +73,7 @@ async function searchId(){
     try {
         const response = await fetch(url, options);
         const result = await response.text();
-        afficherResultat(result)
-
+        afficherResultatIdorClass(result)
         console.log(result);
     } catch (error) {
         console.error(error);
@@ -100,8 +99,10 @@ async function searchClass(){
     try {
         const response = await fetch(url, options);
         const result = await response.text();
+        afficherResultatIdorClass(result)
 
-        afficherResultat(result)
+
+      
         console.log(result);
     } catch (error) {
         console.error(error);
@@ -111,36 +112,96 @@ async function searchClass(){
 
 
 function afficherResultat(result){
+    
+    clearResult();
+
 	result = JSON.parse(result);
 	console.log(result);
 	
 	let resultDiv = document.getElementById('result');
+    resultDiv.classList.remove('hidden');
+
 	for (let i = 0; i < result.data.length; i++) {
+    const conteneur = document.createElement("div");
 	const titre = document.createElement("h2");
 	const image = document.createElement("img");
 	const synopsis = document.createElement("p");
 	const genre = document.createElement("p");
 	const ranking = document.createElement("p");
 	const episodes = document.createElement("p");
-
+    resultDiv.appendChild(conteneur);
 	titre.textContent = result.data[i].title;
-	resultDiv.appendChild(titre);
+	conteneur.appendChild(titre);
 	image.src = result.data[i].image;
-	resultDiv.appendChild(image);
-	synopsis.textContent = "Synopsis : " + result.data[i].synopsis;
-	resultDiv.appendChild(synopsis);
-	genre.textContent = "Genre : " + result.data[i].genre;
-	resultDiv.appendChild(genre);
+	conteneur.appendChild(image);
+	synopsis.innerHTML = "<strong>Synopsis :</strong> " + result.data[i].synopsis;
+	conteneur.appendChild(synopsis);
+ 
+	genre.textContent = "Genre : " + result.data[i].genres;
+	conteneur.appendChild(genre);
 	ranking.textContent = "Classement : " + result.data[i].ranking;
-	resultDiv.appendChild(ranking);
+	conteneur.appendChild(ranking);
 	episodes.textContent = "Nombre d'épisodes : " + result.data[i].episodes;
-	resultDiv.appendChild(episodes);
+	conteneur.appendChild(episodes);
+
+    conteneur.className += ' flex flex-col items-center justify-center border-b-2';
+    image.className += 'shadow-lg mt-5'
+    titre.className += ' font-bold text-2xl mb-2 text-center text-justify';
+    episodes.className += 'mb-5'
+    synopsis.className += ' text-justify mt-5 mb-5 md:w-[75%] text-center'
+
 	}
+	
+}
+
+
+function afficherResultatIdorClass(result){
+    
+    clearResult();
+	result = JSON.parse(result);
+	console.log(result);
+	
+	let resultDiv = document.getElementById('result');
+    resultDiv.classList.remove('hidden');
+
+	
+    const conteneur = document.createElement("div");
+	const titre = document.createElement("h2");
+	const image = document.createElement("img");
+	const synopsis = document.createElement("p");
+	const genre = document.createElement("p");
+	const ranking = document.createElement("p");
+	const episodes = document.createElement("p");
+    resultDiv.appendChild(conteneur);
+	titre.textContent = result.title;
+	conteneur.appendChild(titre);
+	image.src = result.image;
+	conteneur.appendChild(image);
+	synopsis.innerHTML = "<strong>Synopsis :</strong> " + result.synopsis;
+	conteneur.appendChild(synopsis);
+ 
+	genre.textContent = "Genre : " + result.genres;
+	conteneur.appendChild(genre);
+	ranking.textContent = "Classement : " + result.ranking;
+	conteneur.appendChild(ranking);
+	episodes.textContent = "Nombre d'épisodes : " + result.episodes;
+	conteneur.appendChild(episodes);
+
+    conteneur.className += ' flex flex-col items-center justify-center border-b-2';
+    titre.className += ' font-bold text-2xl mb-2 text-center text-justify';
+    episodes.className += 'mb-5'
+    synopsis.className += ' text-justify mt-5 mb-5 md:w-[75%]'
+
+	
 	
 }
 
 function clearResult(){
     resultDiv = document.getElementById('result');
-    resultDiv.removeAllChild();
+    while (resultDiv.firstChild) {
+     resultDiv.removeChild(resultDiv.firstChild);
+    }
+
+    resultDiv.className += ' hidden';
 
 }
