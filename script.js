219831e655a71dafa91c74ff53c6fcdcc00fec3e
Matter <antoine.matter@etu.unicaen.fr>
2025-09-29 11:36:37 +0200

    trois fonctions de recherches respectivement tire, id, classement

--- script.js
+++ script.js
@@ -3,20 +3,30 @@ const key = '23581e0e60mshf94250b663de525p1cd632jsn7bbe7b3e4a81';
 const host = 'anime-db.p.rapidapi.com';
 
 
-var par = document.getElementById("opt-select").options[document.getElementById('liste').selectedIndex].text;
 
 const connexionBtn = document.getElementById('connexion');
 
 connexionBtn.addEventListener("click",()=>{
+    var par = document.getElementById("opt-select").options[document.getElementById('opt-select').selectedIndex].text;
+
     event.preventDefault();
     console.log("Btn connexion")
     console.log(par);
+
+    if(par == 'Titre'){
+        searchTitle();
+    }else if(par == 'Identifiant' ){
+        searchId();
+    }else if (par == 'Classement'){
+        searchClass();
+    }else{
+        alert('veuillez entrer le type de recherche !')
+    }
     
-   // SearchTitle();
 })
 
 
-async function SearchTitle(){
+async function searchTitle(){
 
     const name = document.getElementById('champsTitre').value;
     console.log(name);
@@ -38,5 +48,54 @@ try {
 } catch (error) {
 	console.error(error);
 }
+    
+};
+
+async function searchId(){
+
+
+    const id = document.getElementById('champsTitre').value;
+    console.log(id);
+    const url = `https://anime-db.p.rapidapi.com/anime/by-id/${id}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': key,
+            'x-rapidapi-host': host
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+    
+};
+
+
+async function searchClass(){
+
+
+    const rank = document.getElementById('champsTitre').value;
+    console.log(rank);
+    const url = `https://anime-db.p.rapidapi.com/anime/by-ranking/${rank}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': key,
+            'x-rapidapi-host': host
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
     
 };
